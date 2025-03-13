d06bafada1964c3c8f18040e679d14608d7d1587
emmaenglishwilkins <emmawilkins@penguincodingschool.com>
2025-03-13 18:42:39 -0400

    highlight selected cell

--- visualizer.py
+++ visualizer.py
@@ -11,6 +11,7 @@ cell_size = width / 9
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 GRAY = (200, 200, 200)
+BLUE = (48,197,255, 128)
 
 font = pygame.font.SysFont('arial', 40)
 
@@ -32,6 +33,14 @@ def draw_board(board):
             thickness = 1
         pygame.draw.line(screen, BLACK, (i * cell_size, 0), (i * cell_size, height), thickness)
         pygame.draw.line(screen, BLACK, (0, i * cell_size), (width, i * cell_size), thickness)
+    
+    # highlight selected cell 
+    if selected_cell: 
+        transparent_surface = pygame.Surface((cell_size, cell_size), pygame.SRCALPHA)
+        transparent_surface.fill(BLUE)
+        screen.blit(transparent_surface, (selected_cell[1] * cell_size, selected_cell[0] * cell_size))
+        #pygame.draw.rect(screen, BLUE, (selected_cell[1] * cell_size, selected_cell[0] * cell_size, cell_size, cell_size), 5)
+
 
 board = Board()
 board.generate_board()
@@ -43,11 +52,27 @@ board.remove_tiles(80)
 # else:
 #     print("too many removed")
 
+selected_cell = None
 running = True
+
 while running: 
     for event in pygame.event.get():
+        # closing window
         if event.type == pygame.QUIT: 
             running = False
+        # selecting cell
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            row = int(y // cell_size)
+            col = int(x // cell_size)
+            # check if valid
+            selected_cell = (row, col)
+        # typing number
+        elif event.type == pygame.KEYDOWN:
+            if selected_cell: 
+                pass
+
+
     draw_board(board.board)
     pygame.display.flip()
 
